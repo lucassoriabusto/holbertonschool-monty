25dd8e8222ab8e85bdff375c2c6376de19cabb12
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-12-17 14:26:23 -0800

    read files have been added

--- monty.c
+++ monty.c
@@ -4,16 +4,24 @@
  */
 int main(int argc; char **argv)
 {
+	FILE *fo = NULL;
+	size_t size = 0;
 	char *buf;
 	int fo, fr;
+	char *imput, *token = NULL;
 
-	fo = open( argv[1], O_RDONLY);
+
+	fo = open(argv[1], O_RDONLY);
 	if(fo == -1)
 	{
 		print(STDERR_FILENO"Error: Can't open file %s\n");
 		exit(EXIT_FAILURE)
 	}
-	else
+	
+	while(getline(&imput, size,fo) == -1)
+	{
+		token = strtok(imput; " \t\n");
 		fr = read(fo, buf);
-		
+		imput++;
+	}
 }
